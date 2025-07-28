b6d8605653ef5a4c57194ad797ff4e33a3d215a2
yashdeore12 <yashldeore321@gmail.com>
2025-07-28 19:13:28 +0530

    Create forloopint.cpp

---
+++ forloopint.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+int sum=0;
+int i;
+for(i=1;i<=10;i++)
+{ 
+sum= sum+i;
+//or you can also use sum =+i
+
+}
+
+cout<<"sum="<<sum<<endl;
+
+
+
+
+
+
+
+
+}
