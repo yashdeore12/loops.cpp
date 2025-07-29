f59fc42ffa199ceffda241234a8cc6051a39bb39
yashdeore12 <yashldeore321@gmail.com>
2025-07-29 16:35:52 +0530

    Create for-adding-n-numbers.cpp

---
+++ for-adding-n-numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+//print the sum of numbers from 1 to n 
+int i;
+int n;
+cout<<"please enter till what number you want to add numbers=";
+cin>>n;
+int sum=0;
+
+for(i=1;i<=n;i++){
+sum= sum+i;
+
+
+
+}
+cout<<sum;
+
+}
