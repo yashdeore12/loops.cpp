5bf5f091b99d3c3b1bbb632852bc197b33597d99
yashdeore12 <yashldeore321@gmail.com>
2025-07-31 09:29:46 +0530

    Create flag-while.cpp

---
+++ flag-while.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n ; 
+bool flag= false;
+cout<<"enter the number  between 2 to 7  ";
+while(!flag){
+
+  cin>>n;
+  if(n>2&&n<7){
+
+cout<<"thank you very much meow meow"<<endl;
+flag = true;
+
+  }
+
+  else{
+    cout<<"please try again ";
+  }
+
+
+}
+
+}
